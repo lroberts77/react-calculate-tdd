f51aacd5f0f1e0a165c879f7ecdbec6f4b137cd2
liam roberts <liamroberts77@googlemail.com>
2020-07-20 20:56:30 +0100

    refactored app.spec to create wrapper object before test

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -3,8 +3,13 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
+  
+  let wrapper;
+
+  beforeEach(() => wrapper = shallow(<App />));
+
   it('should render a <div />', () => {
-    const wrapper = shallow(<App />);
+    // const wrapper = shallow(<App />);
     expect(wrapper.find('div').length).toEqual(1);
   });
 });
