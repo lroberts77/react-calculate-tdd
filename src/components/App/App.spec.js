26144ab3fb1a9a06acc89eee4da43fd8e36aa218
liam roberts <liamroberts77@googlemail.com>
2020-07-20 21:11:37 +0100

    created calculator files and added test for div in Calculator component

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -14,6 +14,6 @@ describe('App', () => {
   });
 
   it('should render the Calculator Component', () => {
-    expect(wrapper.containsMatchingElement(<Caluculator />)).toEqual(true)
+    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true)
   });
 });
