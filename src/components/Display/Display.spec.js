11d064e00b4c5d8a95354dc6ff96f1442f7fb1da
liam roberts <liamroberts77@googlemail.com>
2020-07-22 16:47:06 +0100

    added displayValue prop to beforeeach in display.spec

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -6,7 +6,7 @@ describe('Display', () => {
 
   let wrapper;
 
-  beforeEach(() => wrapper = shallow(<Display displayValue={''} />));
+  beforeEach(() => wrapper = shallow(<Display />));
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
