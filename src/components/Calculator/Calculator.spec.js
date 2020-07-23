263bd9aa0acce2996d7a7b648c2918d4b7afb2dd
liam roberts <liamroberts77@googlemail.com>
2020-07-23 16:03:27 +0100

    added snapshot test for calculator

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -10,6 +10,9 @@ describe('Calculator', () => {
 
   beforeEach(() => wrapper = shallow(<Calculator />));
 
+  it('should render correctly', () => 
+  expect(wrapper).toMatchSnapshot());
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   })
