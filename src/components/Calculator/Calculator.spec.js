325cb707f46419ca96c4febfffa5dd9acce2d393
liam roberts <liamroberts77@googlemail.com>
2020-07-23 15:32:38 +0100

    imported Keypad into calculator.spec

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Calculator from './Calculator';
 import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad';
 
 describe('Calculator', () => {
 
