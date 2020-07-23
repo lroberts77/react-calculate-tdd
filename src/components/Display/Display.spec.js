a004623a37c0ced3dc06782aa7dcfddd62b3f019
liam roberts <liamroberts77@googlemail.com>
2020-07-23 11:52:48 +0100

    added test snapshot in Display.spec

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -8,6 +8,8 @@ describe('Display', () => {
 
   beforeEach(() => wrapper = shallow(<Display  displayValue={''} />));
 
+  it('sould render correctly', () => expect(wrapper).toMatchSnapshot());
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
