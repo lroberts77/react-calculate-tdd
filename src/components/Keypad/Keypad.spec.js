6d35eeae669796d8660d6610c82e8df4c4014b12
liam roberts <liamroberts77@googlemail.com>
2020-07-29 12:12:37 +0100

    added snapshot for keypad

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -18,19 +18,12 @@ describe('Keypad', () => {
     );
   });
 
+  it('should render correctly', () => 
+  expect(wrapper).toMatchSnapshot());
+
   it('should render 4 <div /> \'s ', () => {;
     expect(wrapper.find('div').length).toEqual(4);
   });
-
-  // it('renders the values of numbers', () => {
-  //   wrapper.setProps({numbers: ['0', '1', '2']});
-  //   expect(wrapper.find('.numbers-container').text()).toEqual('012');
-  // });
-
-  // it('renders the values of operators', () => {
-  //   wrapper.setProps({operators: ['+', '-', '*', '/']});
-  //   expect(wrapper.find('.operators-container').text()).toEqual('+-*/')
-  // });
 
   it('should render an instance of the key component for each index of numbers, operators and the submit key', () => {
     const numbers = ['0', '1'];
