ec86fd51e679b8b75a4c6a05c1057ec9488c16db
liam roberts <liamroberts77@googlemail.com>
2020-07-29 12:17:55 +0100

    added number and operator values to the state in calculator.js

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -5,17 +5,18 @@ import './Calculator.css';
 
 class Calculator extends Component {
   state = {
-    //value to be displayed in <Display />
+    // value to be displayed in <Display />
     displayValue: '0',
-    //values to be displayed in number <keys />
-    numbers: [],
-    //values to be displayed in operator <keys />
-    operators: [],
-    //operator selected for math operation
+    // values to be displayed in number <Keys />
+    numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
+    // values to be displayed in operator <Keys />
+    operators: ['/', 'x', '-', '+'],
+    // operator selected for math operation
     selectedOperator: '',
-    //stored value to use for math operatiion
+    // stored value to use for math operation
     storedValue: '',
   }
+ 
 
   callOperator = () => {
     console.log('call operation');
