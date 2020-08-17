abda1b23f99d28264f154cd21cab39911679d96f
liam roberts <liamroberts77@googlemail.com>
2020-08-17 18:27:38 +0100

    specified deployment to use build directory in travis.yml

---
+++ build/precache-manifest.d0968c033517c6d54137b2c6162f650f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee60567ba52c3ea6ad1d4308c87c4c93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8d671a10e2824d04f13",
+    "url": "/static/css/main.6ea6aa8b.chunk.css"
+  },
+  {
+    "revision": "d3a7eda67065a6c80ecc",
+    "url": "/static/js/2.830be268.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.830be268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d671a10e2824d04f13",
+    "url": "/static/js/main.6f364d6e.chunk.js"
+  },
+  {
+    "revision": "ad1dbbc2b8c0432a6a6f",
+    "url": "/static/js/runtime-main.daccfd51.js"
+  }
+]);
